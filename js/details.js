14cf1588824dbaa300ea40b0cbd968abce22debc
ALegostaeva <sashasbox.forjob@gmail.com>
2022-12-25 11:59:04 +0100

    link db changed

--- js/details.js
+++ js/details.js
@@ -4,16 +4,45 @@ const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
 
 const getDetails = async () => {
-    const res = await fetch(`https://my-json-server.typicode.com/alegostaeva/alegostaeva-dbBooks/cells?name=${id}`);
-    const cell = await res.json();
+    const res = await fetch(`https://alegostaeva.github.io/data/db.json`);
+    const cells = await res.json();
+    
+    let cell = cells.find( cell => cell.name === id);
 
-    const template = `
-        <img src=${cell[0].img} height=100px>
-        <h1>${cell[0].name}</h1>
-        <p>${cell[0].task}</p>
+    console.log(cell);
+
+    if ( cell.img == null) {
+        cell.img = "../assets/a1.png"
+    };
+
+    const cellnameh1 = document.createElement('h2');
+    cellnameh1.textContent = 'hi';
+    container.appendChild(cellnameh1);
+
+    let template = `
+        <img src=${cell.img} height=100px alt="Изображение соты">
+        <span class="fa fa-map-marker location"></span><p>${cell.loc}</p>
+        <h2 class="name_cell">${cell.name}</h2>
+        <p class="task">${cell.task}</p><br/>
+        <p>Следующий ход возможен на одну из сот:</p>
         `
-
+    
         container.innerHTML = template;
+    
+    let nextSteps = document.createElement('div');
+    nextSteps.className = "next-steps";
+    //document.getElementsByClassName("next-steps");
+
+    for ( const i in cell.next ) {
+        const el = document.createElement("a");
+        el.textContent = cell.next[i];
+        el.href = "info.html?id="+ cell.next[i];
+        el.className = "next-steps-links";
+        console.log(el);
+        nextSteps.appendChild(el)
+    }
+    console.log(nextSteps);
+    container.appendChild(nextSteps);
 }
 
 window.addEventListener('DOMContentLoaded', () => getDetails());
