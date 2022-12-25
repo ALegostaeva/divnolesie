f37dc10a1306359ca2b88a462120c83a1af41d31
ALegostaeva <sashasbox.forjob@gmail.com>
2022-12-25 12:12:38 +0100

    db update

--- js/details.js
+++ js/details.js
@@ -42,3 +42,17 @@ const getDetails = async () => {
 }
 
 window.addEventListener('DOMContentLoaded', () => getDetails());
+
+
+// Menu
+const body = document.querySelector('body');
+const menuOpener = document.querySelector('[data-js-menu]');
+const menuIcon = menuOpener.querySelector('.fa');
+
+menuOpener.addEventListener('click', function () {
+	menuIcon.classList.toggle('fa-bars');
+	menuIcon.classList.toggle('fa-times');
+
+	console.log(body);
+	body.classList.toggle('-menu-open');
+});
