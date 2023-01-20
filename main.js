103a8a97729104fd61466cf9dc179474d8f802b5
ALegostaeva <sashasbox.forjob@gmail.com>
2023-01-20 10:28:54 +0100

    fix

--- main.js
+++ main.js
@@ -135,19 +135,6 @@ window.onload = function(){
   drawMap(canvas.width, canvas.height, r, false); 
 };
 
-// Menu
-/* const body = document.querySelector('body');
-const menuOpener = document.querySelector('[data-js-menu]');
-const menuIcon = menuOpener.querySelector('.fa');
-console.log('menu vars', body, menuOpener, menuIcon);
-
-menuOpener.addEventListener('click', function () {
-    console.log('tap menu');
-    menuIcon.classList.toggle('fa-bars');
-    menuIcon.classList.toggle('fa-times');
-    body.classList.toggle('-menu-open');
-});
-*/
 
 //переменные для управления кнопками "Скрыть/показать названия ячеек"
 const btnShowLabels = document.querySelector('.addLabels');
