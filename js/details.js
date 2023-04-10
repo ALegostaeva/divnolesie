13eb1028413a4dcf1432b37cfb27fde182fae9d6
ALegostaeva <sashasbox.forjob@gmail.com>
2023-04-10 10:49:04 +0200

    fix bug

--- js/details.js
+++ js/details.js
@@ -5,43 +5,41 @@ const container = document.querySelector('.details');
 
 const getDetails = async () => {
     const res = await fetch(`https://divnolesie.pages.dev/data/db.json`);
-    console.log('await get');
     const cells = await res.json();
-    console.log('await parse',cells);
     let cell = await cells.find( cell => cell.name === id);
     
 
-    locKey = toString(cell.loc);
+    locKey = cell.loc;
     switch (locKey) {
-        case locKey.includes("Снежный перевал"):
+        case "Снежный перевал":
             cell.class='snow';
             cell.color = "#2d3c51";
             cell.bckg = "radial-gradient(67.84% 58.16% at 50% 41.84%, rgba(200, 254, 251, 1) 0%, rgba(45, 60, 81, 1) 60.94%)";
             cell.textColor = '#646464';
             cell.horizon = '../assets/hor_snow.png';
             break;
-        case locKey.includes("Пустыня"):
+        case "Пустыня":
             cell.class='desert';
             cell.color = "#e9b973";
             cell.bckg = "radial-gradient(50% 64.75% at 50% 35.25%, #6D7880 0%, #1D2F36 100%)";
             cell.textColor = 'white';
             cell.horizon = '../assets/hor_desert.png';
             break;
-        case locKey.includes("Междумирье"):
+        case "Междумирье":
             cell.class='fireflies';
             cell.color = "#154225";
             cell.bckg = 'radial-gradient(50% 64.75% at 50% 35.25%, rgba(238, 238, 160, 1) 0%, rgba(28, 73, 60, 1) 100%)';
             cell.horizon = '../assets/hor_fireflies.png';
             createFireflies();
             break;
-        case locKey.includes("Мавкино болото"):
+        case "Мавкино болото":
             cell.class='rain';
             cell.color = "#244530";
             cell.bckg = 'radial-gradient(47.28% 53.35% at 50% 46.65%, rgba(243, 255, 247, 1) 0%, rgba(91, 106, 122, 1) 100%)';
             cell.horizon = '../assets/hor_boloto.png';
             createRain();
             break;
-        case locKey.includes("Каменное плато"):
+        case "Каменное плато":
             cell.class='stones';
             cell.color = "#09090F";
             cell.bckg = "radial-gradient(50% 64.75% at 50% 35.25%, rgba(238, 170, 231, 1) 0%, rgba(32, 49, 59, 1) 100%)";
@@ -55,6 +53,7 @@ const getDetails = async () => {
             cell.horizon = '../assets/hor_fireflies.png';
             createFireflies();
     }
+
 
 
     let template = `
