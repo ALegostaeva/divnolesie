59d6e025180479f01b1b7be82e9e49db7733db73
ALegostaeva <sashasbox.forjob@gmail.com>
2024-02-03 18:07:34 +0100

    solving problem with a blank page in safari

--- main.js
+++ main.js
@@ -125,7 +125,8 @@ function pixel_to_hex(x,y)
 };
 
 function openDescription(address){
-  let pageDescription = window.open("pages/info.html?id="+ address,"_top", address);
+  window.location.href = "pages/info.html?id=" + address;
+  //let pageDescription = window.open("pages/info.html?id="+ address,"_top", address);
   //pageDescription.ta
   //pageDescription.window.open()
 };
