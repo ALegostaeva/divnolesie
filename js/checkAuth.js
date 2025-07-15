7dc64bece378f2b27c74460a31521db5fbe12843
ALegostaeva <sashasbox.forjob@gmail.com>
2025-07-15 14:48:50 +0200

    fix

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -50,6 +50,10 @@
         document.body.appendChild(overlay);
         console.log("here2" );
 
+        var script1 = document.createElement('script');
+        script1.src = 'https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js';
+        document.body.appendChild(script1);
+
         var script = document.createElement('script');
         script.src = 'auth.js';
         document.body.appendChild(script);
